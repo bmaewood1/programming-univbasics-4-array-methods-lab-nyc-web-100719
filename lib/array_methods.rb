29f835bcfe0bb99bff68ecb8e4a74b87bce73bc5
Bailey Wood <woodbmae@gmail.com>
2019-09-17 19:54:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,16 +2,6 @@ def using_include(array, element)
   array.include?(element)
 end
 
-describe "#using_sort" do 
-  it "takes in an argument of an array and returns the sorted array." do 
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    expect(using_sort(array).last).to eq("wow")
-    expect(using_sort(array).first).to eq("I")
-  end
-
-
-
-
 def using_sort(array)
   array.sort
 end
